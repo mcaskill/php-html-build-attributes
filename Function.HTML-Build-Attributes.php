320b509a3b1d7107cfd4a8ea272c077b213b55a5
Chauncey McAskill <chauncey@locomotive.ca>
2021-03-02 12:09:47 -0500

    Improve attribute value handling
    Changed:
    - Replaced `array_map()` with `foreach` to maintain support for array keys while allowing easy exclusion of attributes.
    
    Fixed:
    - Superfluous spaces by ignoring NULL values.
    - Support for boolean values from callable values.
    - Support for numeric (integer and float) values not always rendering.

--- Function.HTML-Build-Attributes.php
+++ Function.HTML-Build-Attributes.php
@@ -15,53 +15,58 @@ if (!function_exists('html_build_attributes')) {
             return '';
         }
 
-        $html = array_map(
-            function ($val, $key) use ($callback) {
-                if (is_bool($val)) {
-                    return ($val ? $key : '');
-                } elseif (isset($val)) {
-                    if ($val instanceof Closure) {
-                        $val = $val();
-                    } elseif ($val instanceof JsonSerializable) {
-                        $val = json_encode(
-                            $val->jsonSerialize(),
-                            (JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE)
-                        );
-                    } elseif (is_callable([ $val, 'toArray' ])) {
-                        $val = $val->toArray();
-                    } elseif (is_callable([ $val, '__toString' ])) {
-                        $val = strval($val);
-                    }
+        $html = [];
+        foreach ($attr as $key => $val) {
+            if (is_null($val)) {
+                continue;
+            }
 
-                    if (is_array($val)) {
-                        if (function_exists('is_blank')) {
-                            $filter = function ($var) {
-                                return !is_blank($var);
-                            };
-                        } else {
-                            $filter = function ($var) {
-                                return !empty($var) || is_numeric($var);
-                            };
-                        }
-                        $val = implode(' ', array_filter($val, $filter));
-                    }
-
-                    if (is_callable($callback)) {
-                        $val = call_user_func($callback, $val);
-                    } elseif (function_exists('esc_attr')) {
-                        $val = esc_attr($val);
-                    } else {
-                        $val = htmlspecialchars($val, ENT_QUOTES);
-                    }
-
-                    if (is_string($val)) {
-                        return sprintf('%1$s="%2$s"', $key, $val);
-                    }
+            if (is_object($val)) {
+                if ($val instanceof Closure) {
+                    $val = $val();
+                } elseif ($val instanceof JsonSerializable) {
+                    $val = json_encode(
+                        $val->jsonSerialize(),
+                        (JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE)
+                    );
+                } elseif (is_callable([ $val, 'toArray' ])) {
+                    $val = $val->toArray();
+                } elseif (is_callable([ $val, '__toString' ])) {
+                    $val = strval($val);
                 }
-            },
-            $attr,
-            array_keys($attr)
-        );
+            }
+
+            if (is_bool($val)) {
+                $html[] = ($val ? $key : '');
+                continue;
+            }
+
+            if (is_array($val)) {
+                if (function_exists('is_blank')) {
+                    $filter = function ($var) {
+                        return !is_blank($var);
+                    };
+                } else {
+                    $filter = function ($var) {
+                        return !empty($var) || is_numeric($var);
+                    };
+                }
+                $val = implode(' ', array_filter($val, $filter));
+            }
+
+            if (is_callable($callback)) {
+                $val = call_user_func($callback, $val);
+            } elseif (function_exists('esc_attr')) {
+                $val = esc_attr($val);
+            } else {
+                $val = htmlspecialchars($val, ENT_QUOTES);
+            }
+
+            if (is_string($val) || is_numeric($val)) {
+                $html[] = sprintf('%1$s="%2$s"', $key, $val);
+                continue;
+            }
+        }
 
         return implode(' ', $html);
     }
